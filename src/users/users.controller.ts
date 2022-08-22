ff2a2a05149f3598426ef5ac5aef775b815b3b09
Colt Skeen <colt.skeen@gesa.com>
2022-08-22 16:43:47 -0700

    added methods and error handling

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Patch, Param, Query, Post, NotFoundException } from '@nestjs/common';
 import { CreateUserDTO } from './DTO/create-user.dto';
+import { UpdateUserDto } from './DTO/update-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
@@ -10,5 +11,27 @@ export class UsersController {
     @Post('/signup')
     createUser(@Body() body: CreateUserDTO){
         this.usersService.create(body.email, body.password);
+    }
+
+    @Get('/:id')
+    async findUser(@Param('id') id: string){
+        const user = await this.usersService.findOne(parseInt(id));
+        if(!user) throw new NotFoundException('user not found');
+        return user;
+    }
+
+    @Get()
+    findAllUsers(@Query('email') email: string) {
+        return this.usersService.find(email);
+    }
+
+    @Patch('/:id')
+    updateUser(@Param('id') id: string, @Body() body: UpdateUserDto){
+        return this.usersService.update(parseInt(id), body);
+    }
+
+    @Delete('/:id')
+    removeUser(@Param('id') id: string){
+        return this.usersService.remove(parseInt(id));
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
@@ -10,8 +10,44 @@ export class UsersService {
     constructor(@InjectRepository(User) private repo: Repository<User>){}
 
     create(email: string, password: string){
+        //CREATES THE INSTANCE
         const user = this.repo.create({email, password});
 
+        //PERSISTS THE INSTANCE TO THE DB
         return this.repo.save(user);
+    }
+
+    findOne(id: number) {
+        return this.repo.findOne({where: { id }});
+    }
+
+    find(email:string) {
+        return this.repo.find({where: { email }});
+    }
+
+    /** 
+     * Partial is a type helper from Typescript that says the type has part of the attributes from the Users class
+     * EX: you can pass {email: 'asdf@asdf.com'} with no password or the password and no email or both or none.
+     **/
+    
+    async update(id: number, attrs: Partial<User>) {
+        //NOTE: Finding a user from the DB is async operation
+        //STEP 1: Find the user you want to update in the DB
+        const user = await this.findOne(id);
+
+        //STEP 2: Check if the user you looked for exists, if not throw an error
+        if (!user) throw new NotFoundException('user not found');
+
+        //STEP 3: copy the changed attributes to the user instance
+        Object.assign(user, attrs);
+
+        //STEP 4: Save your changes to the DB
+        return this.repo.save(user);
+    }
+
+    async remove(id: number) {
+        const user = await this.findOne(id);
+        if(!user) throw new NotFoundException('user not found');
+        return this.repo.remove(user);
     }
 }
